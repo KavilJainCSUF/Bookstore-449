66f1a84e4633ebd279f58b9f5db7c1fa5ca2a910
KavilJainCSUF <kavil.jain@csu.fullerton.edu>
2023-05-16 22:10:39 -0700

    Project done

--- app.py
+++ app.py
@@ -1,136 +1,224 @@
-from fastapi import FastAPI, HTTPException
-from pymongo import MongoClient
-from pymongo.errors import PyMongoError
-from bson import ObjectId
+from bson.regex import Regex
+from fastapi import FastAPI,Depends,HTTPException
 from pydantic import BaseModel
-from typing import List
+import uvicorn
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
-
-# MongoDB connection
-client = MongoClient("mongodb://localhost:27017")
-db = client["bookstore"]
-collection = db["books"]
-
-
+#Pydantic model for the book data 
 class Book(BaseModel):
     title: str
     author: str
     description: str
     price: float
     stock: int
+app = FastAPI()
+
+#connect to MongoDB
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+database= client["bookstore"]
+collection = database["books"]
+
+# Create an index on the "title" field
+collection.create_index("title")
+
+# Create a compound index on the "title" and "author" fields
+collection.create_index([("title", 1), ("author", 1), ("price",1),('stock',1)])
 
 
+
+
+# fetch data from database
+@app.get("/books")
+async def get_books():
+    books = []
+    async  for book in collection.find({}, {"_id": 0}):  # Exclude the _id field
+        books.append(book)
+    return books
+
+# search book by title, author and price range
+@app.get("/search")
+async def search(title: str = None, author: str = None,
+                 min_price: float = None, max_price: float = None):
+    books = await search_books(title, author, min_price, max_price)
+    return {"books": books}
+
+#Add a new book with validation
 @app.post("/books")
 async def create_book(book: Book):
-    book_data = book.dict()
     try:
-        result = collection.insert_one(book_data)
-        book_id = str(result.inserted_id)
-        return {"message": "Book created successfully", "book_id": book_id}
-    except PyMongoError as e:
-        raise HTTPException(status_code=500, detail="Failed to create book") from e
+        book_data = book.dict()
+        if book_data["stock"] < 0:
+            raise HTTPException(status_code=400, detail="Stock must be a non-negative value")
+        if book_data["price"] < 0:
+            raise HTTPException(status_code=400, detail="Price must be a non-negative value")
+        if any(value is None or value.strip() == "" for value in book_data.values() if isinstance(value, str)):
+            raise HTTPException(status_code=400, detail="Empty string values are not allowed")
+        await collection.insert_one(book_data)
+        return {"message": "Book created successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-
-@app.get("/books/{book_id}")
-async def get_book(book_id: str):
-    try:
-        if not ObjectId.is_valid(book_id):
-            raise HTTPException(status_code=400, detail="Invalid book_id")
-
-        book = collection.find_one({"_id": ObjectId(book_id)})
-        if book:
-            book_dict = {
-                "title": book["title"],
-                "author": book["author"],
-                "price": book['price'],
-                "stock": book['stock']
-            }
-            return book_dict
-        else:
-            raise HTTPException(status_code=404, detail="Book not found")
-    except PyMongoError as e:
-        print("Error fetching book:", str(e))
-        raise HTTPException(status_code=500, detail="Failed to fetch book") from e
-
-
-
+#Update an existing book by id
 @app.put("/books/{book_id}")
 async def update_book(book_id: str, book: Book):
-    book_data = book.dict()
-    try:
-        result = collection.update_one({"_id": ObjectId(book_id)}, {"$set": book_data})
-        if result.modified_count == 1:
-            return {"message": "Book updated successfully"}
-        else:
-            raise HTTPException(status_code=404, detail="Book not found")
-    except PyMongoError as e:
-        raise HTTPException(status_code=500, detail="Failed to update book") from e
-
-
+    updated_book = book.dict()
+    result = await collection.update_one({"_id": ObjectId(book_id)}, {"$set": updated_book})
+    #book["_id"] = str(book["_id"])
+    if result.modified_count == 1:
+        return {"message": "Book updated successfully"}
+    else:
+        return {"message": "Book not found"}
+    
+#delete a book
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: str):
+    result = await collection.delete_one({"_id": ObjectId(book_id)})
+    if result.deleted_count == 1:
+        return {"message": "Book deleted successfully"}
+    else:
+        return {"message": "Book not found"}
+#Aggregation
+###################################
+#search total number of books
+@app.get("/stats/total-books")
+async def get_total_books():
+    pipeline = [{"$count": "total_books"}]
+    result = await collection.aggregate(pipeline).to_list(length=None)
+    if result:
+        return result[0]["total_books"]
+    else:
+        return 0
+
+#top selling books
+@app.get("/stats/best-selling")
+async def get_best_selling_books():
+    pipeline = [
+        {"$group": {"_id": "$title", "totalSold": {"$sum": "$stock"}}},
+        {"$sort": {"totalSold": -1}},
+        {"$limit": 5}
+    ]
+    result = await collection.aggregate(pipeline).to_list(length=None)
+    return result
+    
+#top selling author
+@app.get("/stats/top-authors")
+async def get_top_authors():
+    pipeline = [
+        {"$group": {"_id": "$author", "totalBooks": {"$sum": 1}}},
+        {"$sort": {"totalBooks": -1}},
+        {"$limit": 5}
+    ]
+    result = await collection.aggregate(pipeline).to_list(length=None)
+    return result
+############################
+
+
+#fetch a book by id
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: str):
+
     try:
-        result = collection.delete_one({"_id": ObjectId(book_id)})
-        if result.deleted_count == 1:
-            return {"message": "Book deleted successfully"}
+        book = await collection.find_one({"_id": ObjectId(book_id)})
+        if book:
+            book["_id"] = str(book["_id"])
+            return book
         else:
             raise HTTPException(status_code=404, detail="Book not found")
-    except PyMongoError as e:
-        raise HTTPException(status_code=500, detail="Failed to delete book") from e
+    except:
+        raise HTTPException(status_code=400, detail="Invalid book ID")
+    
+######################
+'''
+You will use MongoDB's query operators to implement the following search functionality:
+
+●	Search for books by title
+●	Search for books by author
+●	Search for books by price range
+'''
 
 
-@app.get("/books")
-async def get_all_books():
-    try:
-        books = collection.find()
-        books_list = []
-        for book in books:
-            book["_id"] = str(book["_id"])
-            books_list.append(book)
-        return books_list
-    except PyMongoError as e:
-        raise HTTPException(status_code=500, detail="Failed to fetch books") from e
+######################
+async def get_books_by_title(title: str):
+    # Perform a case-insensitive search for books by author name
+    query = {"title": {"$regex": f".{title}.", "$options": "i"}}
+    books_cursor = collection.find(query)
+
+    # Convert the MongoDB documents to Book objects
+    book_objects = []
+    async for book in books_cursor:
+        book_objects.append(Book(**book))
+
+    return book_objects
+
+@app.get("/books/by_title/{title}")
+async def search_books_by_title(title: str):
+    books = await get_books_by_title(title)
+    if books:
+        return {"books": books}
+    else:
+        raise HTTPException(status_code=404, detail="Book not found")
+##################
+async def get_books_by_author(author: str):
+    # Perform a case-insensitive search for books by author name
+    query = {"author": {"$regex": f".{author}.", "$options": "i"}}
+    books_cursor = collection.find(query)
+
+    # Convert the MongoDB documents to Book objects
+    book_objects = []
+    async for book in books_cursor:
+        book_objects.append(Book(**book))
+
+    return book_objects
+
+@app.get("/books/by_author/{author}")
+async def search_books_by_author(author: str):
+    books = await get_books_by_author(author)
+    if books:
+        return {"books": books}
+    else:
+        raise HTTPException(status_code=404, detail="Book not found")
+################
+async def get_books_by_price_range(min_price: float, max_price: float):
+    # Perform a search for books within the specified price range
+    query = {"price": {"$gte": min_price, "$lte": max_price}}
+    books_cursor = collection.find(query)
+
+    # Convert the MongoDB documents to Book objects
+    book_objects = []
+    async for book in books_cursor:
+        book_objects.append(Book(**book))
+
+    return book_objects
+@app.get("/books/by_price_range/")
+async def search_books_by_price_range(min_price: float, max_price: float):
+    books = await get_books_by_price_range(min_price, max_price)
+    return {"books": books}
 
 
-@app.get("/search")
-async def search_books(title: str = None, author: str = None, min_price: float = None, max_price: float = None):
+#search by path
+async def search_books(title: str = None, author: str = None,
+                        min_price: float = None, max_price: float = None):
     query = {}
+
     if title:
         query["title"] = {"$regex": title, "$options": "i"}
+
     if author:
         query["author"] = {"$regex": author, "$options": "i"}
-    if min_price is not None and max_price is not None:
-        query["price"] = {"$gte": min_price, "$lte": max_price}
-    elif min_price is not None:
-        query["price"] = {"$gte": min_price}
-    elif max_price is not None:
-        query["price"] = {"$lte": max_price}
 
-    try:
-        books = collection.find(query)
-        book_list = [book_dict(book) for book in books] 
-        return book_list
-    except PyMongoError as e:
-        print("Error searching books:", str(e))
-        raise HTTPException(status_code=500, detail="Failed to search books") from e
+    if min_price is not None or max_price is not None:
+        query["price"] = {}
 
-@app.get("/aggregation/stats")
-async def get_statistics():
-    total_books = await collection.count_documents({})
-    best_selling_books = await collection.find().sort("stock", -1).limit(5).to_list(length=None)
-    top_authors = await collection.aggregate([
-        {"$group": {"_id": "$author", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 5}
-    ]).to_list(length=None)
-    return {
-        "total_books": total_books,
-        "best_selling_books": best_selling_books,
-        "top_authors": top_authors
-    }
+        if min_price is not None:
+            query["price"]["$gte"] = min_price
 
+        if max_price is not None:
+            query["price"]["$lte"] = max_price
 
-def book_dict(book):
-    book["_id"] = str(book["_id"])
-    return book 
+    books_cursor = collection.find(query)
+    book_objects = []
+    async for book in books_cursor:
+        book_objects.append(Book(**book))
+
+    return book_objects
